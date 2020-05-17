1a97f49da708d263c06420fa9c91498c2bef7943
Samarth Srivastava <59834694+Samarth484@users.noreply.github.com>
2020-05-18 04:00:39 +0530

    main files uploaded

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+var newItem = [];
+var workItems = [];
+
+app.get("/", function(req, res) {
+  // console.log(req.body);
+
+  var today = new Date();
+  var currentDay = today.getDay();
+  var other = today.getFullYear()
+
+  options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+  day = today.toLocaleDateString("en-US", options);
+
+
+
+  // res.send("<h1>Kya yaar, no chutti!</h1>");
+  res.render("list", {
+    ListTitle: day,
+    newListItem: newItem
+  });
+
+  app.post("/", function(req, res) {
+    console.log(req.body);
+    let item = req.body.newItem;
+    if (req.body.list === "Work") {
+      workItems.push(item);
+      res.redirect("/work");
+    } else {
+      newItem.push(item);
+      res.redirect("/");
+    }
+
+  });
+})
+
+
+app.get("/work", function(req, res) {
+  console.log(req.body);
+  res.render("list", {
+    ListTitle: "Work List",
+    newListItem: workItems
+  });
+});
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server is up and running on port: 3000");
+});
